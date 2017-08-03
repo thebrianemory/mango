201e8ec7c3c1487d0ff0542b02681a78ac94f26d
thebrianemory <the@brianemory.com>
2017-08-02 20:35:17 -0700

    Complete User Story #2 part 1

---
+++ test/mango_web/acceptance/category_page_test.exs
@@ -0,0 +1,59 @@
+defmodule MangoWeb.Acceptance.CategoryPageTest do
+  use ExUnit.Case
+  use Hound.Helpers
+
+  hound_session()
+
+  setup do
+    ## GIVEN ##
+    # There are two products Apple and Tomato priced 100 and 50
+    # categorized under 'fruits' and 'vegetables' respectively
+    :ok
+  end
+
+  test "show fruits" do
+    ## WHEN ##
+    # I navigate to the fruits page
+    navigate_to("/categories/fruits")
+
+    ## THEN ##
+    # I expect the page title to be "Seasonal products"
+    page_title = find_element(:css, ".page-title") |> visible_text()
+    assert page_title == "Fruits"
+
+    # And I expect Tomato in the products displayed
+    product = find_element(:css, ".product")
+    product_name = find_within_element(product, :css, ".product-name") |> visible_text()
+    product_price = find_within_element(product, :css, ".product-price") |> visible_text()
+
+    assert product_name == "Apple"
+    # And I expect its price to be displayed on the screen
+    assert product_price == "100"
+
+    # And I expect that Tomato is not present on the screen
+    refute page_source() =~ "Tomato"
+  end
+
+  test "show vegetable" do
+    ## WHEN ##
+    # I navigate to the vegetables page
+    navigate_to("/categories/vegetables")
+
+    ## THEN ##
+    # I expect the page title to be "Seasonal products"
+    page_title = find_element(:css, ".page-title") |> visible_text()
+    assert page_title == "Vegetables"
+
+    # And I expect Tomato in the products displayed
+    product = find_element(:css, ".product")
+    product_name = find_within_element(product, :css, ".product-name") |> visible_text()
+    product_price = find_within_element(product, :css, ".product-price") |> visible_text()
+
+    assert product_name == "Tomato"
+    # And I expect its price to be displayed on the screen
+    assert product_price == "50"
+
+    # And I expect that Apple is not present on the screen
+    refute page_source() =~ "Apple"
+  end
+end
