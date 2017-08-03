c1e3a6f9bf9e98ad8b428fdd5d6d756f1b879d45
thebrianemory <the@brianemory.com>
2017-08-02 22:10:39 -0700

    Complete User Story #2 part 2

--- test/mango_web/acceptance/homepage_test.exs
+++ test/mango_web/acceptance/homepage_test.exs
@@ -1,14 +1,22 @@
 defmodule MangoWeb.HomepageTest do
-  use ExUnit.Case
+  use Mango.DataCase
   use Hound.Helpers
 
   hound_session()
 
-  test "presence of seasonal products" do
+  setup do
     ## GIVEN ##
     # There are two products Apple and Tomato price 100 and 50 respectively
     # Where Apple being the only seasonal product
+    alias Mango.Repo
+    alias Mango.Catalog.Product
 
+    Repo.insert %Product{ name: "Tomato", price: 50, is_seasonal: false }
+    Repo.insert %Product{ name: "Apple", price: 100, is_seasonal: true }
+    :ok
+  end
+
+  test "presence of seasonal products" do
     ## WHEN ##
     # I navigate to the homepage
     navigate_to("/")
