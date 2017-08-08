c19ca753f093ef48aaba1ce02b6ba1bb25328871
thebrianemory <the@brianemory.com>
2017-08-08 10:11:49 -0700

    Update reg ctrl to use a changeset for :new

--- lib/mango_web/controllers/registration_controller.ex
+++ lib/mango_web/controllers/registration_controller.ex
@@ -1,7 +1,9 @@
 defmodule MangoWeb.RegistrationController do
   use MangoWeb, :controller
+  alias Mango.CRM
 
   def new(conn, _) do
-    render(conn, "new.html")
+    changeset = CRM.build_customer()
+    render(conn, "new.html", changeset: changeset)
   end
 end
