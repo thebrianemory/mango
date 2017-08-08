6d29f524ba757d2ed2fe017d42974e21da1efc3e
thebrianemory <the@brianemory.com>
2017-08-08 09:14:48 -0700

    Update customer email to use citext

--- priv/repo/migrations/20170808143330_create_customers.exs
+++ priv/repo/migrations/20170808143330_create_customers.exs
@@ -2,9 +2,10 @@ defmodule Mango.Repo.Migrations.CreateCustomers do
   use Ecto.Migration
 
   def change do
+    execute "CREATE EXTENSION IF NOT EXISTS citext"
     create table(:customers) do
       add :name, :string
-      add :email, :string
+      add :email, :citext
       add :phone, :string
       add :residence_area, :string
       add :password_hash, :string
