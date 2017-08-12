e04cf5e381b84d89a5fc124559cb11e91600017d
thebrianemory <the@brianemory.com>
2017-08-12 16:33:49 -0700

    Create sales.ex for getting/creating carts

---
+++ lib/mango_web/sales/sales.ex
@@ -0,0 +1,13 @@
+defmodule Mango.Sales do
+  alias Mango.Repo
+  alias Mango.Sales.Order
+
+  def get_cart(id) do
+    Order
+    |> Repo.get_by(id: id, status: "In Cart")
+  end
+
+  def create_cart do
+    %Order{status: "In Cart"} |> Repo.insert!()
+  end
+end
