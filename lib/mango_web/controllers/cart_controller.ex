7e4a2775365f223ce20e4189ace48f2599573b8f
thebrianemory <the@brianemory.com>
2017-08-12 20:46:35 -0700

    Update show to handle cart_changeset

--- lib/mango_web/controllers/cart_controller.ex
+++ lib/mango_web/controllers/cart_controller.ex
@@ -21,6 +21,7 @@ defmodule MangoWeb.CartController do
 
   def show(conn, _params) do
     cart = conn.assigns.cart
-    render conn, "show.html", cart: cart
+    cart_changeset = Sales.change_cart(cart)
+    render conn, "show.html", cart: cart, cart_changeset: cart_changeset
   end
 end
