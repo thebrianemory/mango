0531ee1921a3c3d72e54f3f92bb0ce1949321f3a
thebrianemory <the@brianemory.com>
2017-08-12 17:18:54 -0700

    Update Sales module with add_to_cart function

--- lib/mango_web/sales/sales.ex
+++ lib/mango_web/sales/sales.ex
@@ -10,4 +10,26 @@ defmodule Mango.Sales do
   def create_cart do
     %Order{status: "In Cart"} |> Repo.insert!()
   end
+
+  def add_to_cart(%Order{line_items: []} = cart, cart_params) do
+    attrs = %{line_items: [cart_params]}
+    update_cart(cart, attrs)
+  end
+
+  def add_to_cart(%Order{line_items: existing_line_items} = cart, cart_params) do
+    new_item = %{
+      product_id: String.to_integer(cart_params["product_id"]),
+      quantity: String.to_integer(cart_params["quantity"])
+    }
+    existing_line_items = existing_line_items |> Enum.map(&Map.from_struct/1)
+
+    attrs = %{line_items: [new_items | existing_line_items]}
+    update_cart(cart, attrs)
+  end
+
+  def update_cart(cart, attrs) do
+    cart
+    |> Order.changeset(attrs)
+    |> Repo.update
+  end
 end
