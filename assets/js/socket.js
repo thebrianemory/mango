dd093ffe812e21163b5eb6420edf41f22aeed469
thebrianemory <the@brianemory.com>
2017-08-20 09:48:54 -0700

    Part 2 Mango Robot

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -11,4 +11,21 @@ channel.join()
   })
   .receive("error", resp => { console.log("Unable to join", resp) })
 
+channel.on("message:new", payload => {
+  renderMessage(payload, "MangoRobot")
+})
+
+let chatMessages = $("#pos-chat-room")
+
+let renderMessage = (payload, user) => {
+  let template = document.createElement("li")
+  template.innerHTML = `<div class="chat-body">
+    <div class="header">
+      <strong class="primary-font">${user}</strong>
+    </div>
+    <p>${payload.message}</p>
+  </div>`
+  chatMessages.append(template);
+}
+
 export default socket
